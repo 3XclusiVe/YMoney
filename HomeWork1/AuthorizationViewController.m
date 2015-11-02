2b1beb318a25cacfd8ae66754b1d339cbe5adc83
Дмитрий <malllkin007@gmail.com>
2015-11-02 23:55:34 +0300

    Настроил сессию авторизации яндекса.

--- HomeWork1/AuthorizationViewController.m
+++ HomeWork1/AuthorizationViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "AuthorizationViewController.h"
+#import "YMAConstants.h"
+#import "YMAExternalPaymentSession.h"
+#import "YMAAPISession.h"
 
 @interface AuthorizationViewController ()
 
 - (IBAction)autorizationSuccess:(id)sender;
 
-@property (weak, nonatomic) IBOutlet UIWebView *authorizationWebView;
+@property(weak, nonatomic) IBOutlet UIWebView *authorizationWebView;
 
 
 - (IBAction)authorizationFailed:(id)sender;
@@ -21,13 +24,27 @@
 
 @implementation AuthorizationViewController
 
+//My client id.
+NSString *_clientId = @"CBE42B5C0151CE4F2AC277F5A037A45DF265B83F21EB4FF9D61A559D2A73DBF6";
+//URI that the OAuth server sends the authorization result to.
+NSString *_redirectUri = @"http://ya.ru";
+//A list of requested permissions.
+NSString *_permissions = @"account-info operation-history";
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    NSURL *websiteUrl = [NSURL URLWithString:@"http://www.google.com"];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:websiteUrl];
-    [_authorizationWebView loadRequest:urlRequest];
-    // Do any additional setup after loading the view.
+
+    NSDictionary * additionalParameters = @{
+            YMAParameterResponseType : YMAValueParameterResponseType, //Constant parameter
+            YMAParameterRedirectUri : _redirectUri,
+            YMAParameterScope : _permissions,
+    };
+
+    YMAAPISession *session = [[YMAAPISession alloc] init];
+    NSURLRequest *authorizationRequest = [session authorizationRequestWithClientId:_clientId
+                                                              additionalParameters:additionalParameters];
+    [_authorizationWebView loadRequest:authorizationRequest];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,9 +55,11 @@
 - (IBAction)autorizationSuccess:(id)sender {
     NSLog(@"authorization success");
 }
+
+// Тестовое использование делегата.
 - (IBAction)authorizationFailed:(id)sender {
-    id<ChildViewControllerDelegate> strongDelegate = self.delegate;
-    
+    id <ChildViewControllerDelegate> strongDelegate = self.delegate;
+
     if ([strongDelegate respondsToSelector:@selector(childViewController:didChooseValue:)]) {
         [strongDelegate childViewController:self didChooseValue:5];
     }
