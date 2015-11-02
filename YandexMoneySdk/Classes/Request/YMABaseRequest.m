cb470ce2eda248df0e0d449433f80256c230b6c7
Дмитрий <malllkin007@gmail.com>
2015-11-03 00:23:49 +0300

    Добавил SDK в репозиторий.

---
+++ YandexMoneySdk/Classes/Request/YMABaseRequest.m
@@ -0,0 +1,46 @@
+//
+//  YMABaseRequest.m
+//
+//  Created by Alexander Mertvetsov on 01.11.13.
+//  Copyright (c) 2013 Yandex.Money. All rights reserved.
+//
+
+#import "YMABaseRequest.h"
+#import "YMABaseResponse.h"
+
+@implementation YMABaseRequest
+
+- (void)buildResponseWithData:(NSData *)data
+                      headers:(NSDictionary *)headers
+                        queue:(NSOperationQueue *)queue
+                   completion:(YMARequestHandler)block
+{
+    NSOperation *operation =
+    [self buildResponseOperationWithData:data
+                                 headers:(NSDictionary *)headers
+                              completion:^(YMABaseResponse *response, NSError *error) {
+                                  block(self, response, error);
+        }];
+
+    if (operation == nil) {
+        block(self, nil, [NSError errorWithDomain:YMAErrorDomainUnknown code:0 userInfo:nil]);
+        return;
+    }
+
+    [queue addOperation:operation];
+}
+
+- (NSOperation *)buildResponseOperationWithData:(NSData *)data
+                                        headers:(NSDictionary *)headers
+                                     completion:(YMAResponseHandler)handler
+{
+    NSString *reason = [NSString stringWithFormat:@"%@ must be ovverriden", NSStringFromSelector(_cmd)];
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:reason userInfo:nil];
+}
+
+- (YMARequestMethod)requestMethod
+{
+    return YMARequestMethodPost;
+}
+
+@end
