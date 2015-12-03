a5597afee63d0d2b2e44dee6b148737f51ca9b30
Дмитрий <malllkin007@gmail.com>
2015-12-03 15:38:56 +0300

    Исправлена ошибка с неправильным путем к bridge-header

--- Sources/Application/Presentation/Spinner/YMoney-Bridging-Header.h
+++ Sources/Application/Presentation/Spinner/YMoney-Bridging-Header.h
@@ -1,4 +1,13 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  YMoney-Bridging-Header.h
+//  YMoney
+//
+//  Created by Дмитрий on 03.12.15.
+//  Copyright © 2015 DMA. All rights reserved.
 //
 
+#ifndef YMoney_Bridging_Header_h
+#define YMoney_Bridging_Header_h
+
+
+#endif /* YMoney_Bridging_Header_h */
