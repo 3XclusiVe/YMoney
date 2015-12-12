95d7cda6ae23129dec74c8b8a8a114e5c3ba1a5d
Дмитрий <malllkin007@gmail.com>
2015-12-12 15:42:11 +0300

    Внешние библиотеки перемещены в отдельную группу. Проведена чистка проекта.

--- Sources/Application/Presentation/Spinner/CustomSpinner.m
+++ Sources/Application/Presentation/Spinner/CustomSpinner.m
@@ -34,21 +34,6 @@ static volatile BOOL isShowing;
         }
     }];
     
-    
-    /**dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, ^ {
-        [NSThread sleepForTimeInterval:timeout];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if(isShowing) {
-                [[SwiftSpinner show:label animated:YES] addTapHandler:^{
-                    [SwiftSpinner hide: ^ {
-                        isShowing = NO;
-                    }];
-                }subtitle:labelAfterTimeout];
-            }
-        });
-    });**/
-    
 }
 
 +(void) hide:(NSString *) completionLabel {
